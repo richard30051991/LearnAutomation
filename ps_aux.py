bfff0beabf07f6e77e2a1c65195d0dcf539e62dd
Richard <richard30051991@gmail.com>
2022-05-12 13:39:39 +0300

    Работа с ОС

--- ps_aux.py
+++ ps_aux.py
@@ -7,10 +7,13 @@ dt_string = dt.strftime("%d-%m-%Y-%H:%M")
 files = (sys.stdout, open(f"{dt_string}", "w"))
 
 ps = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).communicate()[0]
+#ps = "USER PID %CPU %MEM VSZ RSS TTY STAT START TIME COMMAND\nroot 1 0.0 0.3 5748 3512 pts/0 Ss 07:29 0:00 sh space\nagr 51 0.0 0.2 5748 3592 pts/1 Ss+ 07:34 0:00 bash\nroot 65 0.0 0.1 9388 2896 pts/0 R+ 09:18 0:00 ps aux"
 processes = ps.decode().split("\n")
 nfields = len(processes[0].split()) - 1
 processes_running = []
 CPU, MEM, NAME_CPU_MAX, NAME_MEM_MAX, users, list_process = 0, 0, None, None, [], []
+CPU_MAX = 0.0
+MEM_MAX = 0.0
 for row in processes[1:]:
     process = row.split(None, nfields)
     list_process.append(process)
@@ -20,15 +23,13 @@ for row in processes[1:]:
     except IndexError:
         break
     processes_running.append(process[1])
-    CPU_MAX = 0
-    MEM_MAX = 0
     CPU = CPU + float(process[2])
     MEM = MEM + float(process[3])
-    if float(process[2]) > CPU_MAX:
+    if float(process[2]) >= float(CPU_MAX):
         CPU_MAX = process[2]
         NAME_CPU_MAX = process[10]
-    if float(process[2]) > MEM_MAX:
-        MEM_MAX = process[2]
+    if float(process[3]) >= float(MEM_MAX):
+        MEM_MAX = process[3]
         NAME_MEM_MAX = process[10]
     name_process_max_mem = "pass"
 
@@ -47,7 +48,7 @@ for f in files:
         print(users[i], ":", total_len_user_process, file=f)
     print(f"Всего памяти используется: {round(MEM, 1)}%", file=f)
     print(f"Всего CPU используется: {round(CPU, 1)}%", file=f)
-    print(f"Больше всего памяти использует: {str(NAME_CPU_MAX)[:20]}", file=f)
-    print(f"Больше всего CPU использует: {str(NAME_MEM_MAX)[:20]}", file=f)
+    print(f"Больше всего памяти использует: {str(NAME_MEM_MAX)[:20]}", file=f)
+    print(f"Больше всего CPU использует: {str(NAME_CPU_MAX)[:20]}", file=f)
 
 sys.stdout.close()
